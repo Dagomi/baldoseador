347886700d03a741cb71e55c26bc92c412f86b0b
Dagomi <xxx@xxx.com>
2017-06-20 17:12:10 +0200

    [Add] config file

---
+++ config.py
@@ -0,0 +1,21 @@
+'''
+
+@author: David Gomez david.gomez@i2cat.com
+
+'''
+
+# A class variable.
+InputhPath =""
+PremierProjectName = ""
+InputJson= ""
+OuputhPath = ""
+#Paths fore share accros the program
+MainPathFolder =""
+WebMediaFolder = ""
+TempFolder = ""
+PortraitFolder=""
+imageFolder=""
+#Folders
+tvFolder = ""
+sphericalFolder = ""
+porttalFolder= ""
